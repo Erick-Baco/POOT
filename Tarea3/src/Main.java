9c1d95e823bc1c1f2fae0f55c16828af213c7e4d
Erick-Baco <jesusbaco542@gmail.com>
2023-02-22 18:18:02 -0600

    tarea 3, cuadro de gato #

---
+++ Tarea3/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        char hor = '\u2500';
+        char ver = '\u2502';
+
+        System.out.print(hor);
+        System.out.print(hor);
+        System.out.print(hor);
+        System.out.print(hor);
+        System.out.println(hor);
+
+        System.out.print(ver);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.println(ver);
+
+        System.out.print(ver);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.println(ver);
+
+        System.out.print(ver);
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.println(ver);
+
+        System.out.print(hor);
+        System.out.print(hor);
+        System.out.print(hor);
+        System.out.print(hor);
+        System.out.println(hor);
+    }
+}
